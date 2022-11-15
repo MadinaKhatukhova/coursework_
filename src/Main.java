bba97c6aee3e1bd1d22839498e2b7871e4d01414
Madina <madina-khat@yandex.ru>
2022-11-15 19:40:14 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,60 @@
+
+import java.util.Arrays;
+
 public class Main {
+    public static Employee[] employees() {
+        Employee[] employee = new Employee[10];
+        employee[0] = new Employee("Иванов", "Иван", "Иванович", 1, 115000);
+        employee[1] = new Employee("Александров", "Александр", "Александрович", 2, 155000);
+        return employee;
+    }
+
+    static Employee[] employee = employees();
+
+    public static void listOfAllEmployees() {
+        System.out.println("Список сотрудников:");
+        for (Employee list : employee) {
+            if (list == null) {
+                break;
+            }
+            System.out.println(list);
+        }
+    }
+
+    public static void salaryExpensesPerMonth() {
+        float sum = 0;
+        for (Employee i : employee) {
+            if (i == null) {
+                continue;
+            }
+            sum += i.salary;
+        }
+        System.out.println("Сумма затрат на зарплаты в месяц: " + sum);
+    }
+
+    public static void minimumSalaryEmployee() {
+        float minSalary = 1_000_000;
+        String fullName = null;
+        int department = 0;
+        int id = 0;
+        for (Employee i : employee) {
+            if (i == null) {
+                break;
+            } else if (i.salary < minSalary) {
+                minSalary = i.salary;
+                fullName = i.fullName;
+                department = i.department;
+                id = i.id;
+            }
+        }
+        System.out.println('\n' + "Минимальная зарплата у сотрудника №" + id + '\n' + "ФИО: " +
+                fullName + '\n' + "Отдел: " + department + '\n' + "Зарплата: " + minSalary);
+
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        listOfAllEmployees();
+        salaryExpensesPerMonth();
+        minimumSalaryEmployee();
     }
 }
